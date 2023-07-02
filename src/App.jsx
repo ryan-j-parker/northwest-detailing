cbc3443e08a1a5dbafb8d44903b042136935d143
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-07-01 23:31:55 -0700

    initialize repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="title">Northwest Detailing</h1>
+      <h4 className="phone-number">(971) 712-9498</h4>
+      <img src="/ai-boat-detail.png" className="hero" />
+    </>
+  )
+}
+
+export default App;
