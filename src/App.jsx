2038c262f79f5f943b60ff0e01d5b2da69f43907
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-07-07 16:46:59 -0700

    css and hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div className="name-number">
           <h1 className="title">Northwest Detailing</h1>
           <h4 className="phone-number">(971) 712-9498</h4>
-          <a href="/info" className="button">
+          <a href="/info">
             <button className="button">Learn more</button>
           </a>
         </div>
