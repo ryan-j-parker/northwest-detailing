b332017776f0789e0e8e7c513baf3a1724fee111
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-07-07 16:34:57 -0700

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-    <div className="name+number">
-      <h1 className="title">Northwest Detailing</h1>
-      <h4 className="phone-number">(971) 712-9498</h4>
-    </div>
-    <img src="/ai-boat-detail.png" className="hero" />
+      <div className="image" />
+      <div className="wrapper">
+        <div className="name-number">
+          <h1 className="title">Northwest Detailing</h1>
+          <h4 className="phone-number">(971) 712-9498</h4>
+          <a href="/info" className="button">
+            <button className="button">Learn more</button>
+          </a>
+        </div>
+        {/* <div className="container">
+          <img src="/ai-boat-detail.png" className="hero" />
+        </div> */}
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
